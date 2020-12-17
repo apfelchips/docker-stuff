becb4b8ea4f10c44e284ac28402252d5cffd2222
Ubuntu <ubuntu@raspi-johannes.fritz.box>
2020-12-17 19:04:08 +0000

    comments++

--- setup_docker_namespaces.sh
+++ setup_docker_namespaces.sh
@@ -3,6 +3,8 @@
 # https://echorand.me/posts/docker-user-namespacing-remap-system-user
 # https://docs.docker.com/engine/security/userns-remap/#prerequisites
 
+# root in docker-container --> UID=100000 on docker-host
+
 sudo usermod --add-subuids 100000-165536 $(id -u -n)
 sudo usermod --add-subgids 100000-165536 $(id -u -n)
 
