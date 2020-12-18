897cbbdac77aff3b0354d81290d895430e1611b9
Ubuntu <ubuntu@raspi-johannes.fritz.box>
2020-12-18 08:36:26 +0000

    moved some stuff around

--- nextcloud/init.sh
+++ nextcloud/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-ENV_FILE="${ENV_FILE:-$(realpath $(dirname $0)/.env)}"
+COMPOSE_PROJECT_DIR="$(realpath $(dirname $0))"
+ENV_FILE="${ENV_FILE:-$COMPOSE_PROJECT_DIR/.env}"
 
 test -f $ENV_FILE && source $ENV_FILE
 
@@ -9,11 +10,10 @@ if [ -z $POSTGRES_PASSWORD ]; then
 	echo "POSTGRES_PASSWORD=\"${RANDOM_DB_PASSWORD}\"" >> $ENV_FILE 
 fi
 
+sudo mkdir -p ${NEXTCLOUD_DATA_DIR:-$COMPOSE_PROJECT_DIR/nextcloud-data}
+sudo chown -R 100082:100000 ${NEXTCLOUD_DATA_DIR:-$COMPOSE_PROJECT_DIR/nextcloud-data}
 
-sudo mkdir -p ${NEXTCLOUD_DATA_DIR:-/var/nextcloud-data}
-sudo chown -R 100082:100000 ${NEXTCLOUD_DATA_DIR:-/var/nextcloud-data}
-
-sudo mkdir -p ${NEXTCLOUD_DB_DIR:-/var/nextcloud-db}
-sudo chown -R 100072:100000 ${NEXTCLOUD_DB_DIR:-/var/nextcloud-db}
+sudo mkdir -p ${NEXTCLOUD_DB_DIR:-/$COMPOSE_PROJECT_DIR/nextcloud-db}
+sudo chown -R 100072:100000 ${NEXTCLOUD_DB_DIR:-$COMPOSE_PROJECT_DIR/nextcloud-db}
 
 docker-compose up --build -d
