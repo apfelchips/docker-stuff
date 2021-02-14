44024efe0801337add5c21278de60b14058faf53
attero <service-github@attero.me>
2021-02-14 18:25:57 +0000

    Update setup_docker_namespaces.sh

--- setup_docker_namespaces.sh
+++ setup_docker_namespaces.sh
@@ -5,8 +5,8 @@
 
 # root in docker-container --> UID=100000 on docker-host
 
-sudo usermod --add-subuids 100000-165536 $(id -u -n)
-sudo usermod --add-subgids 100000-165536 $(id -u -n)
+sudo usermod --add-subuids 100000-165536 $(SUDO_USER)
+sudo usermod --add-subgids 100000-165536 $(SUDO_USER)
 
 echo "{\"userns-remap\":\"$(id -u -n):$(id -u -n)\"}" | sudo tee /etc/docker/daemon.json
 
